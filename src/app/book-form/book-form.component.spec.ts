f2fd2a8877287f45dd5a95479b65d28c1d2e8e9e
Craig <eddycr@state.gov>
2017-07-03 08:02:31 -0400

    Updated Karma tests so that they will pass

--- src/app/book-form/book-form.component.spec.ts
+++ src/app/book-form/book-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms'
 
 import { BookFormComponent } from './book-form.component';
 
@@ -8,6 +9,7 @@ describe('BookFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [ BookFormComponent ]
     })
     .compileComponents();
